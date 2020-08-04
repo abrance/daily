2448cc5a83536f73a486366dd9f36534ca9d3447
abrance <1103098607@qq.com>
2020-08-04 13:47:43 +0800

    finish 归并排序

--- homework/day_b.py
+++ homework/day_b.py
@@ -7,28 +7,34 @@ def merge_sort(ls, l, m, r):
             ls[l] = ls[r]
             ls[r] = t
         return
+    # 赋值一份需要排序的部分给b
     b = []
+    for item in ls[l: r+1]:
+        b.append(item)
+
     # 两边的已经排序好，需要把l, m 的和 m+1, r 的合并
     j, k = l, m+1
     i = l
+
+    # 修改ls中的值，指针在b上移动
     while j <= m and k <= r:
-        if ls[j] <= ls[k]:
-            b.append(ls[j])
+        if b[j-l] <= b[k-l]:
+            ls[i] = b[j-l]
             j += 1
         else:
-            b.append(ls[k])
+            ls[i] = b[k-l]
             k += 1
-        if j == m+1:
-            b += ls[k:]
-            break
-        elif k == r+1:
-            b += ls[j:m+1]
-            break
-
-    for i in range(l, r):
-        ls[i] = b[i]
-    # if len(ls) > r+1:
-    #     ls = b+ls[r+1:]
+        i += 1
+        
+    while j<=m:
+        ls[i] = b[j-l]
+        j += 1
+        i += 1
+        
+    while k<=r:
+        ls[i] = b[k-l]
+        k += 1
+        i += 1
 
 def merge(ls, l, r):
     if l == r:
@@ -40,8 +46,8 @@ def merge(ls, l, r):
 
 
 def main():
-    ls = [1, 22, 13, 4, 5]
-    merge(ls, 0, 4)
+    ls = [1, 22, 13, 77, 10, 59, 4, 5]
+    merge(ls, 0, 7)
     print(ls)
 
 
